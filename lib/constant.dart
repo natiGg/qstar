9d30e3ef30720a5facd4113e6c78ca82ce098c61
natiGg <nathangere12@gmail.com>
2021-09-19 06:58:38 -0700

    some ui change

---
+++ lib/constant.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+Color mBackgroundColor = Color(0xFFFFFFFF);
+
+Color mPrimaryColor = Color(0xFF702963);
+
+Color mPrimaryTextColor = Color(0xFFae3d99);
+
+Color mSecondTextColor = Color(0xFFae3d99);
+
+Color mSecondColor = Color(0xFFC28E79);
+
+Color mFacebookColor = Color(0xFF4277BC);
+
+Color mTitleTextColor = Color(0xFF2D140D);
+
+List<String> coffeeNames = [
+  'Espresso','Cappuccino','Macciato','Mocha','Latte',
+];
---
+++ lib/screen/home/home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:qstar/constant.dart';
+import 'package:qstar/screen/home/widget/login_and_register.dart';
+import 'package:qstar/screen/home/widget/login_with_facebook.dart';
+import 'package:qstar/screen/home/widget/slider_dot.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 80,
+          ),
+          Image.asset('assets/images/q.png'),
+          SliderDot(),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Have Fun\nin town!',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: mPrimaryTextColor,
+              fontSize: 32,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          SizedBox(
+            height: 60,
+          ),
+          LoginAndRegister(),
+          LoginWithFacebook()
+        ],
+      ),
+    );
+  }
+}
+
