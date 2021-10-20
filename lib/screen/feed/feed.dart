185fb117022c865bbbb4198e9455872d8e9eb7ca
nat <nathangere12@gmail.com>
2021-10-20 15:19:26 +0100

    live/photo

--- lib/screen/feed/feed.dart
+++ lib/screen/feed/feed.dart
@@ -332,40 +332,40 @@ class _FeedState extends State<Feed> {
                             ],
                           ),
                        SizedBox(height: 20,),
-                    Container(
-                      height: 40.0,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          FlatButton.icon(
-                            onPressed: () => print('Live'),
-                            icon: const Icon(
-                              Icons.videocam,
-                              color: Colors.red,
-                            ),
-                            label: Text('Live'),
-                          ),
-                          const VerticalDivider(width: 8.0),
-                          FlatButton.icon(
-                            onPressed: () => print('Photo'),
-                            icon: const Icon(
-                              Icons.photo_library,
-                              color: Colors.green,
-                            ),
-                            label: Text('Photo'),
-                          ),
-                          const VerticalDivider(width: 8.0),
-                          FlatButton.icon(
-                            onPressed: () => print('Room'),
-                            icon: const Icon(
-                              Icons.video_call,
-                              color: Colors.purpleAccent,
-                            ),
-                            label: Text('Room'),
-                          ),
-                        ],
-                      ),
-                    ),
+                    // Container(
+                    //   height: 40.0,
+                    //   child: Row(
+                    //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    //     children: [
+                    //       FlatButton.icon(
+                    //         onPressed: () => print('Live'),
+                    //         icon: const Icon(
+                    //           Icons.videocam,
+                    //           color: Colors.red,
+                    //         ),
+                    //         label: Text('Live'),
+                    //       ),
+                    //       const VerticalDivider(width: 8.0),
+                    //       FlatButton.icon(
+                    //         onPressed: () => print('Photo'),
+                    //         icon: const Icon(
+                    //           Icons.photo_library,
+                    //           color: Colors.green,
+                    //         ),
+                    //         label: Text('Photo'),
+                    //       ),
+                    //       const VerticalDivider(width: 8.0),
+                    //       FlatButton.icon(
+                    //         onPressed: () => print('Room'),
+                    //         icon: const Icon(
+                    //           Icons.video_call,
+                    //           color: Colors.purpleAccent,
+                    //         ),
+                    //         label: Text('Room'),
+                    //       ),
+                    //     ],
+                    //   ),
+                    // ),
                   ],
                 ),
               ),
