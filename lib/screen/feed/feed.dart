6f17b8ffd1f2e8845cd276e046ccf2149f22408b
nat <nathangere12@gmail.com>
2021-10-21 16:10:02 +0100

    post modal

--- lib/screen/feed/feed.dart
+++ lib/screen/feed/feed.dart
@@ -96,6 +96,9 @@ class Feed extends StatefulWidget {
 }
 
 class _FeedState extends State<Feed> {
+  FocusNode _focus = new FocusNode();
+  List<String> _animals = ["Friends", "public", "only me"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -168,7 +171,7 @@ class _FeedState extends State<Feed> {
             // ),
 
             Card(
-              margin: EdgeInsets.symmetric(horizontal: 0.0,vertical: 10),
+              margin: EdgeInsets.symmetric(horizontal: 0.0, vertical: 10),
               elevation: 2,
               shape: null,
               child: Container(
@@ -182,10 +185,34 @@ class _FeedState extends State<Feed> {
                             backgroundImage:
                                 AssetImage('assets/images/profile1.jpg')),
                         const SizedBox(width: 8.0),
-                        Expanded(
-                          child: TextField(
-                            decoration: InputDecoration.collapsed(
-                              hintText: 'What\'s on your mind?',
+
+                        Container(
+                          color: Colors.white,
+                          padding: const EdgeInsets.only(left: 10, right: 10),
+                          child: Container(
+                            height: 40,
+                            width: 300,
+                            margin: EdgeInsets.symmetric(vertical: 10),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                border: Border.all(
+                                    color: Colors.grey.withOpacity(0.9))),
+                            child: FlatButton(
+                              onPressed: () {
+                                _postModal(context);
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.only(left: 8.0),
+                                child: Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text(
+                                      'What\'s on your mind?',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey.withOpacity(0.9),
+                                      ),
+                                    )),
+                              ),
                             ),
                           ),
                         ),
@@ -349,7 +376,9 @@ class _FeedState extends State<Feed> {
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
                           FlatButton.icon(
-                            onPressed: () => print('Live'),
+                            onPressed: () {
+                              _postModal(context);
+                            },
                             icon: const Icon(
                               Icons.videocam,
                               color: Colors.red,
@@ -465,6 +494,235 @@ class _FeedState extends State<Feed> {
         ),
       ),
     );
+  }
+
+  void _postModal(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return Container(
+              height: MediaQuery.of(context).size.height,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                            backgroundImage:
+                                AssetImage('assets/images/profile1.jpg')),
+                        const SizedBox(width: 8.0),
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  child: Center(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(2.0),
+                                      child: Text(
+                                        "@Betty",
+                                        style: TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                                                Row(
+                                  children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(2.0),
+                                  child: Container(
+                                    width: 100,
+                                    height: 50,
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 10),
+                                   
+                                    child: Column(
+                                      children: [
+                                        DropdownButton<String>(
+                                          onChanged: (value) {
+                                            setState(() {});
+                                          },
+
+                                          // Hide the default underline
+                                          underline: Container(),
+                                          hint: Align(
+                                              alignment: Alignment.center,
+                                              child: Text(
+                                            'Public',
+                                            style: TextStyle(
+                                              color: mPrimaryColor,
+                                              fontSize: 10
+                                            ),
+                                          )),
+                                          icon: Align(
+                                            alignment: Alignment.topCenter,
+                                            child: Icon(
+                                              Icons.arrow_drop_down,
+                                              color: mPrimaryColor,
+                                              
+                                            ),
+                                          ),
+                                          isExpanded: true,
+
+                                          // The list of options
+                                          items: _animals
+                                              .map((e) => DropdownMenuItem(
+                                                    child: Container(
+                                                      alignment: Alignment
+                                                          .centerLeft,
+                                                      child: Text(
+                                                        e,
+                                                        style: TextStyle(
+                                                            fontSize: 12),
+                                                      ),
+                                                    ),
+                                                    value: e,
+                                                  ))
+                                              .toList(),
+
+                                          // Customize the selected item
+                                          selectedItemBuilder:
+                                              (BuildContext context) =>
+                                                  _animals
+                                                      .map((e) => Center(
+                                                            child: Text(
+                                                              e,
+                                                              style: TextStyle(
+                                                                  fontSize:
+                                                                      18,
+                                                                  color: Colors
+                                                                      .amber,
+                                                                  fontStyle:
+                                                                      FontStyle
+                                                                          .italic,
+                                                                  fontWeight:
+                                                                      FontWeight
+                                                                          .bold),
+                                                            ),
+                                                          ))
+                                                      .toList(),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                  ],
+                                ),
+                               Container(
+                                  padding: const EdgeInsets.only(
+                                      left: 80, right: 10),
+                                  child: Container(
+                                    height: 40,
+                                    width: 80,
+                                    margin: EdgeInsets.symmetric(vertical: 10),
+                                    decoration: BoxDecoration(
+                                        color: mPrimaryColor,
+                                        borderRadius: BorderRadius.circular(5),
+                                        border:
+                                            Border.all(color: mPrimaryColor)),
+                                    child: FlatButton(
+                                      onPressed: () {},
+                                      child: Center(
+                                          child: Text(
+                                        'Post',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
+                                      )),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                
+                      ],
+                    ),
+
+                    const Divider(height: 5.0, thickness: 0.5),
+                    SizedBox(
+                      height: 45,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Container(
+                        height: 50,
+                        child: Expanded(
+                          child: TextField(
+                            decoration: InputDecoration.collapsed(
+                              hintText: 'What\'s on your mind?',
+                            ),
+                          ),
+                        ),
+                      ),
+                      
+                    ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                 
+            
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          FlatButton.icon(
+                            onPressed: () {
+                            },
+                            icon: const Icon(
+                              FontAwesome.video_camera,
+                              color: Colors.red,
+                            ),
+                            label: Text('Add your Video'),
+                          ),
+                          FlatButton.icon(
+                            onPressed: () => print('Photo'),
+                            icon: const Icon(
+                              FontAwesome.photo,
+                              color: Colors.green,
+                            ),
+                            label: Text(' Add Photo'),
+                          ),
+                          FlatButton.icon(
+                            onPressed: () => print('Room'),
+                            icon: const Icon(
+                              FontAwesome.user,
+                              color: mPrimaryColor,
+                            ),
+                            label: Text('Add People'),
+                          ),
+                            FlatButton.icon(
+                            onPressed: () => print('Room'),
+                            icon: const Icon(
+                              FontAwesome.smile_o,
+                              color: Colors.amber,
+                            ),
+                            label: Text('Feeling Activity'),
+                          ),
+                              FlatButton.icon(
+                            onPressed: () => print('Room'),
+                            icon: const Icon(
+                              FontAwesome.location_arrow,
+                              color: Colors.green,
+                            ),
+                            label: Text('Add  Location'),
+                          ),
+                        ],
+                      ),
+                    ),
+                                  ],
+                                ),
+                  ],
+                ),
+              ));
+        });
   }
 }
 
