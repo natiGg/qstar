462820f940e61e338d0fd557c67e6f725b47ecf3
nat <nathangere12@gmail.com>
2021-10-25 16:19:03 +0100

    send message

--- lib/screen/Chat/chat_screen.dart
+++ lib/screen/Chat/chat_screen.dart
@@ -189,78 +189,92 @@ class _ChatScreenState extends State<ChatScreen> {
         elevation: 0,
       ),
       backgroundColor: Colors.transparent,
-      body: ListView.builder(
-          reverse: false,
-          itemCount: messages.length,
-          itemBuilder: (context, int index) {
-            final message = messages[index];
-            bool isMe = message.sender.id == currentUser.id;
-            return Container(
-              margin: EdgeInsets.only(top: 10),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment:
-                        isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      if (!isMe)
-                        CircleAvatar(
-                          radius: 15,
-                          backgroundImage: AssetImage(widget.user.imageUrl),
-                        ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        constraints: BoxConstraints(
-                            maxWidth: MediaQuery.of(context).size.width * 0.6),
-                        decoration: BoxDecoration(
-                            color: isMe ?mPrimaryColor : Colors.grey[200],
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(16),
-                              topRight: Radius.circular(16),
-                              bottomLeft: Radius.circular(isMe ? 12 : 0),
-                              bottomRight: Radius.circular(isMe ? 0 : 12),
-                            )),
-                        child: Text(
-                          messages[index].text,
-                          style: bodyTextMessage.copyWith(
-                              color: isMe ? Colors.white : Colors.grey[800]),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 5),
-                    child: Row(
-                      mainAxisAlignment:
-                          isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                reverse: false,
+                itemCount: messages.length,
+                itemBuilder: (context, int index) {
+                  final message = messages[index];
+                  bool isMe = message.sender.id == currentUser.id;
+                  return Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: Column(
                       children: [
-                        if (!isMe)
-                          SizedBox(
-                            width: 40,
-                          ),
-                        Icon(
-                          Icons.done_all,
-                          size: 20,
-                          color: bodyTextTime.color,
-                        ),
-                        SizedBox(
-                          width: 8,
-                        ),
-                        Text(
-                          message.time,
-                          style: bodyTextTime,
-                        )
+                        Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment:
+                                  isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                if (!isMe)
+                                  CircleAvatar(
+                                    radius: 15,
+                                    backgroundImage: AssetImage(widget.user.imageUrl),
+                                  ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(10),
+                                  constraints: BoxConstraints(
+                                      maxWidth: MediaQuery.of(context).size.width * 0.6),
+                                  decoration: BoxDecoration(
+                                      color: isMe ?mPrimaryColor : Colors.grey[200],
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(16),
+                                        topRight: Radius.circular(16),
+                                        bottomLeft: Radius.circular(isMe ? 12 : 0),
+                                        bottomRight: Radius.circular(isMe ? 0 : 12),
+                                      )),
+                                  child: Text(
+                                    messages[index].text,
+                                    style: bodyTextMessage.copyWith(
+                                        color: isMe ? Colors.white : Colors.grey[800]),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 5),
+                              child: Row(
+                                mainAxisAlignment:
+                                    isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+                                children: [
+                                  if (!isMe)
+                                    SizedBox(
+                                      width: 40,
+                                    ),
+                                  Icon(
+                                    Icons.done_all,
+                                    size: 20,
+                                    color: bodyTextTime.color,
+                                  ),
+                                  SizedBox(
+                                    width: 8,
+                                  ),
+                                  Text(
+                                    message.time,
+                                    style: bodyTextTime,
+                                  )
+                                ],
+                              ),
+                            ),
+                   
+                          ],
+          
+                        ),   
                       ],
                     ),
-                  )
-                ],
-              ),
-            );
-          }),
+                      
+                  );
+                }),
+          ),
+          _buildMessageComposer()
+        ],
+      ),
     );
   }
 }
